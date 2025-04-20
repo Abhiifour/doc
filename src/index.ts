c9ccce764113a2079e5153a3a7579a4e3d2f0de9
Abhishek Singh <as8998013@gmail.com>
2025-04-20 21:58:46 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from "express"
+import mongoose from "mongoose"
+
+const app = express()
+
+    
+const dbURI: string = process.env.DATABASE_URL || "mongodb://localhost:27017/defaultdb";
+
+mongoose.connect(dbURI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+
+// Define schema
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  name: String,
+  password: String,
+});
+
+// Compile model from schema
+const SomeModel = mongoose.model("User", User);
+
+
+app.get("/",(req,res) => {
+    res.json({
+        "message":"Hello World!"
+    })
+})
+
+app.post("/",(req,res) => {
+    res.json({
+        "message":"Hello World!"
+    })
+})
+
+app.listen(3000,() => {
+    console.log("server running on port 3000")
+})
